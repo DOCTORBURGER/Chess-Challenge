25dc5b52754370a7b5740b7d2bc799b5b211abfd
Jonah Bogusch <jonahmbog@gmail.com>
2023-07-23 23:26:50 -0500

    Added M1 and random moves functionality.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,33 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
+    private int[] pieceValues = { 0, 1, 3, 3, 5, 8 };
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] allLegalMoves = board.GetLegalMoves();
+
+        Random rng = new();
+        Move moveToPlay = allLegalMoves[rng.Next(allLegalMoves.Length)];
+
+        foreach (var move in allLegalMoves)
+        {
+            if (MoveIsCheckmate(board, move))
+            {
+                moveToPlay = move;
+            }
+        }
+
+        return moveToPlay;
+    }
+
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
